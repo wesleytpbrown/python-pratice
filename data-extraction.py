2e9b320e43e986d652a7a32a1ed3610b3545c7fb
Trey Brown <treybrown3293@gmail.com>
2024-11-04 14:07:18 -0500

    data extraction

---
+++ data-extraction.py
@@ -0,0 +1,30 @@
+import os
+
+
+#list to store data
+file_list = []
+
+
+#get the list of files
+for file_name in os.listdir():
+    if os.path.isfile(file_name):
+
+        #get file extension
+        file_extension = file_name.split('.')[-1] if '.' in file_name else 'Unknown'
+
+        #get file size
+        file_size = os.path.getsize(file_name)
+
+        #dictionary for file data
+        file_metadata = {
+            'file_name': file_name,
+            'file_size_bytes': file_size,
+            'file_extension': file_extension
+        }
+
+
+        file_list.append(file_metadata)
+
+
+for file in file_list:
+    print(file)
