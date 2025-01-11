37655707031399fb7e4a52db020b349f9d408aed
Trey Brown <treybrown3293@gmail.com>
2025-01-11 09:39:33 -0500

    Add files via upload

---
+++ docker notes.py
@@ -0,0 +1,10 @@
+import docker
+
+
+#Docker commands
+
+
+docker container run -it #start a new container interactively
+docker container exec -it # run additional command in the existing container
+
+docker container logs #log information about the container
