461ad5bc605e350b46ca75e1cb59daf7ec2a4ac8
Trey Brown <treybrown3293@gmail.com>
2025-01-11 10:17:29 -0500

    Update docker notes.py

--- docker notes.py
+++ docker notes.py
@@ -10,5 +10,13 @@ docker container exec -it # run additional command in the existing container
 docker container logs #log information about the container
 
 docker container run -p  80:80#expose the port
+docker container port #shows exposed ports
+
+docker container inspect --format #common option for formatting the output of commands
 
 
+docker network ls #show networks
+docker network inspect #inspect a network
+docker network create --driver #create a network
+docker network connect #attach a netwrok to a container
+docker network disconnect #detach a network from a container
